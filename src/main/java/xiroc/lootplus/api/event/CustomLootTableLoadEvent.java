7059abb92d51750a02d18beacc52c63b4dc1426d
XYROC <xiroc.minecraft@gmail.com>
2019-06-04 12:37:06 +0200

    0.1.1

--- src/main/java/xiroc/lootplus/api/event/CustomLootTableLoadEvent.java
+++
@@ -1,7 +0,0 @@
-package xiroc.lootplus.api.event;
-
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-public class CustomLootTableLoadEvent extends Event {
-
-}
--- src/main/java/xiroc/lootplus/util/Reference.java
+++ src/main/java/xiroc/lootplus/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "lootplus";
 	public static final String NAME = "LootPlus";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.1.1";
 	public static final String MINECRAFT_VERSIONS = "[1.12],[1.12.1],[1.12.2]";
 
 }
